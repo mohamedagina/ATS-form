205aafbf26c6b2fe329e966d2df3d32407c1fe13
mohamedagina <mohamedagina001@gmail.com>
2023-09-25 17:43:48 +0300

    feat: Add basic information panels

--- src/pages/content-management/ContentManagement.tsx
+++ src/pages/content-management/ContentManagement.tsx
@@ -1,12 +1,55 @@
 import { useSearchParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
-import { Tabs, CoverUpload } from '../../components';
+import { Tabs, CoverUpload, Panel, InfoSummary } from '../../components';
 import './ContentManagement.css';
+import { SummaryOptions } from '../../models';
 
 const ApplicationForm = () => {
+  const { personalInformation, profile } = useSelector(
+    (state: RootState) => state.application.data.attributes
+  );
   return (
     <div className="application-form">
       <CoverUpload />
+      <Panel title="Personal Information">
+        <div className="information-panel">
+          {Object.keys(personalInformation).map(
+            infoTitle =>
+              infoTitle !== 'personalQuestions' && (
+                <InfoSummary
+                  key={infoTitle}
+                  title={infoTitle}
+                  category="personalInformation"
+                  options={
+                    personalInformation[
+                      infoTitle as keyof typeof personalInformation
+                    ] as SummaryOptions
+                  }
+                />
+              )
+          )}
+        </div>
+      </Panel>
+
+      <Panel title="Profile Information">
+        <div className="information-panel">
+          {Object.keys(profile).map(
+            infoTitle =>
+              infoTitle !== 'profileQuestions' && (
+                <InfoSummary
+                  key={infoTitle}
+                  title={infoTitle}
+                  category="profile"
+                  options={
+                    profile[infoTitle as keyof typeof profile] as SummaryOptions
+                  }
+                />
+              )
+          )}
+        </div>
+      </Panel>
     </div>
   );
 };
